f0a388c88ae12d9191484dec2aa46708bf125aaa
AlissaStorm <Alissaalidib@hotmail.com>
2025-03-25 09:20:43 +0100

    Prepared the setup for the project to build web application

---
+++ src/main/java/app/Controllers/RoutingController.java
@@ -0,0 +1,19 @@
+package app.Controllers;
+
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+
+
+public class RoutingController {
+
+
+
+    public static void startRouting(Javalin app, ConnectionPool connectionPool){
+
+        app.get("/", ctx ->  ctx.render("index.html"));
+
+
+
+    }
+
+}
---
+++ src/main/java/app/Main.java
@@ -0,0 +1,35 @@
+package app;
+
+import app.Controllers.RoutingController;
+import app.config.SessionConfig;
+import app.config.ThymeleafConfig;
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
+
+public class Main {
+
+
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "Cupcake";
+
+    public static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
+
+    public static void main(String[] args) {
+
+        // Initializing Javalin and Jetty webserver
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            config.jetty.modifyServletContextHandler(handler ->  handler.setSessionHandler(SessionConfig.sessionConfig()));
+            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+        }).start(7070);
+
+
+        RoutingController.startRouting(app, connectionPool);
+
+    }
+}
