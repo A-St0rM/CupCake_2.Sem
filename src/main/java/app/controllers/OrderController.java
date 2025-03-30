901f33da7f5ed292a95554bd17457a8813903b31
AlissaStorm <Alissaalidib@hotmail.com>
2025-03-30 17:02:19 +0200

    Started on order mapper and controller. Started also on status since they are dependent on each other

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -4,7 +4,7 @@ public class OrderController {
 
     //TODO: get all orders with corresponding customers. We need to make DTO for these.
 
-    //TODO: change status on each order
+    //TODO: change status on each order (Maybe in status controller/mapper)
 
 
 }
